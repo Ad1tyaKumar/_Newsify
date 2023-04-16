7bd6a1db9e2dbb69e77a7e7117e9ea3611492039
Aditya <adityakumar2003birth@gmail.com>
2023-04-16 17:19:43 +0530

    changes in features.js

--- server/utils/features.js
+++ server/utils/features.js
@@ -5,9 +5,9 @@ export const sendCookie = (user, res, message, statusCode = 200) => {
 
     res.status(statusCode)
         .cookie('token', token, {
-            httpOnly: true, expires: new Date(Date.now() + 15 * 60 * 1000),
-            sameSite:process.env.NODE_ENV==="Development"?"lax":"none",
-            secure:process.env.NODE_ENV==="Development"?false:true,
+            httpOnly: true, expires: new Date(Date.now() + 7*24*60 * 60 * 1000),
+            sameSite:"none",
+            secure:true,
         })
         .json({
             success: true,
